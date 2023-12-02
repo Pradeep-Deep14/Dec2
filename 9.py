c701501def457e55768afa488a57e1ac90b2bcf6
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-12-02 17:15:04 +0530

    first commit

---
+++ 9.py
@@ -0,0 +1,9 @@
+a=[]
+b=[a.append(i) for i in range(5)]
+print(a)
+print(b)
+
+#for loop ku munadi a.append(i)
+#will take place so print b none nu varudhu
+#[0, 1, 2, 3, 4]
+#[None, None, None, None, None]
